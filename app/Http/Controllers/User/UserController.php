7b1bb5491acb4edcb756d1c3d8dce36563c60786
Akanbi Ibrahim <realakanbi@gmail.com>
2019-11-09 21:36:30 +0100

    finished the user controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -20,15 +20,7 @@ class UserController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -39,6 +31,20 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $rules=[
+            'name'=>'required',
+            'email'=>'required|email|unique:users',
+            'password'=>'required|min:3|confirmed'
+
+        ];
+        $this->validate($request,$rules);
+        $data=$request->all();
+        $data['password']=bcrypt($request->password);
+        $data['verified']=User::UNVERIFIED_USER;
+        $data['verification_token']=User::generateVerificationCode();
+        $data['admin']=User::REGULAR_USER;
+        $user=User::create($data);
+        return response()->json(['user'=>$user],201);
     }
 
     /**
@@ -47,21 +53,13 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
         //
+        return response()->json(['user'=>$user],201);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -73,6 +71,42 @@ class UserController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $user=User::findOrFail($id);
+        $rules=[
+            'email'=>'email|unique:users,email,' .$user->id,
+            'password'=>'min:3|confirmed',
+            'admin'=>'in:'.User::ADMIN_USER.','.User::REGULAR_USER
+
+        ];
+        $this->validate($request,$rules);
+        if($request->has('name'))
+        {
+            $user->name=$request->name;
+        }
+        if($request->has('email') && $user->email != $request->email)
+        {
+            $user->verified=User::UNVERIFIED_USER;
+            $user->verfication_token=User::generateVerificationCode();
+            $user->email=$request->email;
+        }
+        if($request->has('password'))
+        {
+            $user->password=bcrypt($request->password);
+        }
+        if($request->has('admin'))
+        {
+            if(!$user->isVerified())
+            {
+                return response()->json(['error'=>'You must be verified to make this change','code'=>401],401);
+            }
+            $user->admin=$request->admin;
+        }
+        if(!$user->isDirty())
+        {
+            return response()->json(['error'=>'You need to make update','code'=>402],402);
+        }
+        $user->save();
+        return response()->json(['user'=>$user],200);
     }
 
     /**
@@ -81,8 +115,12 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
         //
+       // $user=User::findOrFail($id);
+        $user->delete();
+        return response()->json(['status'=>'User deleted'],200);
+
     }
 }
